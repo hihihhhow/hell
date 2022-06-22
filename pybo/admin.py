bcf2d39f8e916abef6d0427b9283e4c98c15f0a7
윤혜인 <yhi05231@gmail.com>
2022-06-23 00:25:48 +0900

    커밋

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from.models import Question
+from.models import Question, Answer, Comment, Category
 
 class QuestionAdmin(admin.ModelAdmin):
     search_fields = ['subject']
 
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
+admin.site.register(Comment)
+admin.site.register(Category)
+
 # Register your models here.
